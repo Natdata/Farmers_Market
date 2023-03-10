21237b264cd7332fe9b75a32e8c9a9f2b3fbcc1e
Natalia Pijanowska <125644275+Natdata@users.noreply.github.com>
2023-03-10 19:39:31 +0100

    Flaga_kukurydza
    Dodanie flagi sprzedaży kukurydza, aby przeanalizować wpływ jej występowania na zakup świeżych produktów

--- Farmers_Market_projekt.sql
+++ Farmers_Market_projekt.sql
@@ -68,6 +68,28 @@ FROM customer_purchases cp
 GROUP BY
     mdi.market_year,
     mdi.market_week;
+    
+-- Ograniczam się do potrzebynych wartości
+-- Stosuje case oraz coalesce, aby uniknąć nulli 
+-- Ustawiam flagę na kukurydzę, żeby zobaczyć czy jej obecność ma wpływ na sprzedaż świeżych produktów
+
+SELECT
+    mdi.market_year,
+    mdi.market_week,
+    COUNT(DISTINCT vi.vendor_id) AS vendor_count,
+    COUNT(DISTINCT vi.product_id) AS unique_product_count,
+    SUM(CASE WHEN p.product_qty_type = 'unit' THEN vi.quantity ELSE 0 END) AS unit_products_qty,
+    SUM(CASE WHEN p.product_qty_type = 'lbs' THEN vi.quantity ELSE 0 END) AS bulk_products_lbs,
+    ROUND(COALESCE(SUM(vi.quantity * vi.original_price), 0), 2) AS total_product_value,
+    MAX(CASE WHEN p.product_id = 16 THEN 1 ELSE 0 END) AS corn_available_flag
+FROM vendor_inventory vi 
+   INNER JOIN product p
+       ON vi.product_id = p.product_id
+   RIGHT JOIN market_date_info mdi
+       ON mdi.market_date = vi.market_date
+GROUP BY
+   mdi.market_year,
+   mdi.market_week;
 
 
 
